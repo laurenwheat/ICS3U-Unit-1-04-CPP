e07211860098bc103bf4dd29f3468404db2c6aa7
Ubuntu <ubuntu@ip-172-31-59-214.ec2.internal>
2021-06-18 15:41:19 +0000

    did basic math

---
+++ basic_math.cpp
@@ -0,0 +1,14 @@
+// Copyright Lauren Wheatley
+//
+// Created by Lauren Wheatley
+// Created on June 2021
+// This program does basic math
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has dimensions:" << std::endl;
+    std::cout << "7cm x 8cm" << std::endl;
+    std::cout << "Area is " << (7*8) << "cm²" << std::endl;
+    std::cout << "Perimeter is " << (2*(7+8)) << "cm" << std::endl;
+}
